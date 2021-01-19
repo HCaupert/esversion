0f613403aeb75bc2debca0e17334c5409a884737
HCaupert <hcaupert@takima.fr>
2021-01-19 10:30:21 +0100

    first commit

---
+++ src/main/java/fr/test/esversion/EsversionApplication.java
@@ -0,0 +1,13 @@
+package fr.test.esversion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsversionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EsversionApplication.class, args);
+    }
+
+}
